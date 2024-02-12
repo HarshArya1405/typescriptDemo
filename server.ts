121b76a04c626ac09a08babfd8c4fbdd79bf6f23
HarshArya1405 <harsh@wemotiveforge.com>
2024-02-12 11:07:18 +0530

    new:added comments for app and server file

--- server.ts
+++ server.ts
@@ -1,23 +1,31 @@
+// Importing necessary modules and packages
 import express, { Application } from 'express';
-import dotenv from 'dotenv';
-import logger from './src/util/logger';
-dotenv.config(); 
 import ENV from './src/config/environments';
 import Server from './src/app';
+import logger from './src/util/logger'; 
+import dotenv from 'dotenv';
+dotenv.config(); // Loading environment variables from .env file into process.env
 
+// Creating an instance of the Express application
 const app: Application = express();
+
+// Initializing the Server class with the Express application instance
 new Server(app);
 
+// Parsing the port number from environment variables
 const PORT: number = parseInt(ENV.servicePort, 10);
 
+// Starting the Express server to listen on the specified port
 app
   .listen(PORT, ENV.baseAppURL, function () {
-    logger.info(`Server is running on port ${PORT}.`);
+    logger.info(`Server is running on port ${PORT}.`); // Logging server startup message
   })
+  // Handling server startup errors
   .on('error', (err: Error) => {
+    // Checking if the error is due to address already in use
     if (err instanceof Error && (err as NodeJS.ErrnoException).code === 'EADDRINUSE') {
-      logger.error('Error: address already in use');
+      logger.error('Error: address already in use'); // Logging address already in use error
     } else {
-      logger.error(err);
+      logger.error(err); // Logging other server startup errors
     }
-  });
+  });  
--- src/app.ts
+++ src/app.ts
@@ -6,25 +6,28 @@ import ENV from './config/environments';
 import { requestLogger } from './util/request.logger';
 
 export default class Server {
-constructor(app: Application) {
-  this.config(app);
-  this.syncDatabase();
-  new Routes(app);
-}
-
-  private config(app: Application): void {
-    const corsOptions: CorsOptions = {
-      origin: ENV.whitelistUrls
-    };
-    app.use(cors(corsOptions));
-    app.use(express.json());
-    app.use(express.urlencoded({ extended: true }));
-    app.use(requestLogger);
-    
+  // Constructor function to initialize the Server class
+  constructor(app: Application) {
+    this.config(app); // Configuring the Express application
+    this.syncDatabase(); // Synchronizing the database
+    new Routes(app); // Initializing routes
   }
 
+  // Function to configure the Express application
+  private config(app: Application): void {
+    // Cors options for handling Cross-Origin Resource Sharing (CORS)
+    const corsOptions: CorsOptions = {
+      origin: ENV.whitelistUrls // Whitelist URLs for CORS
+    };
+    app.use(cors(corsOptions)); // Using CORS middleware
+    app.use(express.json()); // Parsing incoming JSON requests
+    app.use(express.urlencoded({ extended: true })); // Parsing URL-encoded requests
+    app.use(requestLogger); // Logging incoming requests
+  }
+
+  // Function to synchronize the database
   private syncDatabase(): void {
-    const db = new Database();
-    db.sequelize?.sync();
+    const db = new Database(); // Creating a new Database instance
+    db.sequelize?.sync(); // Synchronizing the database
   }
 }
