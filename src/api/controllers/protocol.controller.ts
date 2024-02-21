0a316d1ec56a06864a18887a2d0b60d27a36c63d
Tirthankar231 <tirthankar@wemotiveforge.com>
2024-02-21 18:05:48 +0530

    added the list controller function

--- src/api/controllers/protocol.controller.ts
+++ src/api/controllers/protocol.controller.ts
@@ -1,5 +1,5 @@
 import { Protocol } from '../models/protocol.model';
-import { JsonController, Post } from 'routing-controllers';
+import { JsonController, Post, Get, QueryParams } from 'routing-controllers';
 import { ResponseSchema } from 'routing-controllers-openapi';
 import { ProtocolService } from '../services/protocol.service';
 
@@ -16,5 +16,12 @@ export class ProtocolController {
   @ResponseSchema(Protocol, { isArray: true })
   public async fetchAndDumpData(): Promise<Protocol[]> {
     return await this.protocolService.fetchAndDumpData();
+  }
+  @Get()
+  public async list(
+    @QueryParams() query: { offset?: number; limit?: number; name?: string; category?: string }
+  ): Promise<object> {
+    const { offset = 0, limit = 10, name = '', category = '' } = query;
+    return await this.protocolService.list(offset, limit, name, category);
   }
 }
