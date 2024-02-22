f4495530d09a9856502a68970acab602509c7b54
Tirthankar231 <tirthankar@wemotiveforge.com>
2024-02-22 11:29:19 +0530

    added GetProtocolsQuery class for offset, limit validations

--- src/api/controllers/protocol.controller.ts
+++ src/api/controllers/protocol.controller.ts
@@ -2,6 +2,21 @@ import { Protocol } from '../models/protocol.model';
 import { JsonController, Post, Get, QueryParams } from 'routing-controllers';
 import { ResponseSchema } from 'routing-controllers-openapi';
 import { ProtocolService } from '../services/protocol.service';
+import { IsNotEmpty, IsOptional } from 'class-validator';
+
+class GetProtocolsQuery {
+  @IsOptional()
+  name!: string;
+
+  @IsOptional()
+  category!: string;
+
+  @IsNotEmpty()
+  offset!: number;
+
+  @IsNotEmpty()
+  limit!: number;
+}
 
 @JsonController('/protocol')
 export class ProtocolController {
@@ -17,11 +32,12 @@ export class ProtocolController {
   public async fetchAndDumpData(): Promise<Protocol[]> {
     return await this.protocolService.fetchAndDumpData();
   }
+  
   @Get()
   public async list(
-    @QueryParams() query: { offset?: number; limit?: number; name?: string; category?: string }
+    @QueryParams() query: GetProtocolsQuery
   ): Promise<object> {
-    const { offset = 0, limit = 10, name = '', category = '' } = query;
+    const { offset, limit, name, category } = query;
     return await this.protocolService.list(offset, limit, name, category);
   }
 }
