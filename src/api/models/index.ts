950711b0f012f32e672beb3f1e49afb74f3f45fe
Tirthankar231 <tirthankar@wemotiveforge.com>
2024-02-29 18:41:01 +0530

    NEW : Exported Text model in the index.js

--- src/api/models/index.ts
+++ src/api/models/index.ts
@@ -4,3 +4,4 @@ export { Tag } from './tag.model';
 export { Protocol } from './protocol.model';
 export { OnBoardingFunnel } from './onboardingFunnel';
 export { VideoContent } from './videoContent.model';
+export { Text } from './text.model';
