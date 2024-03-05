54172a3653d6956a5a808605f5b0ee4a0d27df72
Tirthankar231 <tirthankar@wemotiveforge.com>
2024-03-05 15:55:54 +0530

    FIX : Fixed the issue where tags and protocols are not coming, CHG : changed the id type to string, FIX : fixed the issue where tags and protocols are not getting updated, CHG : Modified the list function to add filter of multiple creatorIds

--- src/api/v1/services/videoContent.service.ts
+++ src/api/v1/services/videoContent.service.ts
@@ -1,7 +1,7 @@
 // Import necessary modules and types
 import { VideoContent } from '../../models';
 import { AppDataSource } from '../../../loaders/typeormLoader';
-import { FindManyOptions, Like } from 'typeorm';
+import { FindManyOptions, In } from 'typeorm';
 import { DuplicateRecordFoundError, NoRecordFoundError } from '../../errors';
 import { MESSAGES } from '../../constants/messages';
 
@@ -19,7 +19,7 @@ export class VideoContentService {
      * @param data Array of video content data to import
      * @returns Success message if data is imported successfully
      */
-    public async import(creatorId: number, data: []): Promise<object> {
+    public async import(creatorId: string, data: []): Promise<object> {
         try {
             if (data && data.length > 0) {
                 for (const row of data) {
@@ -39,10 +39,11 @@ export class VideoContentService {
      * @param data Video content data to create
      * @returns The created video content
      */
-    public async create(creatorId: number, data: VideoContent): Promise<VideoContent> {
+    public async create(creatorId: string, data: VideoContent): Promise<VideoContent> {
         try {
             const existingVideoContent = await videoContentRepository.findOne({
-                where: { userId: creatorId, title: data.title }
+                where: { userId: creatorId, title: data.title },
+                relations: ['tags', 'protocols']
             });
 
             if (existingVideoContent) {
@@ -64,7 +65,7 @@ export class VideoContentService {
      * @param videoContentId The ID of the video content
      * @returns The retrieved video content
      */
-    public async get(creatorId: number, videoContentId: number): Promise<VideoContent> {
+    public async get(creatorId: string, videoContentId: string): Promise<VideoContent> {
         try {
             const videoContent = await videoContentRepository.findOne({
                 where: { userId: creatorId, id: videoContentId },
@@ -86,26 +87,31 @@ export class VideoContentService {
      * @param query Object containing limit, offset, and optional title for search
      * @returns Object containing the count and list of video content
      */
-    public async list(creatorId: number, query: { limit?: number, offset?: number, title?: string }): Promise<object> {
+    public async list(searchParams: { title?: string, creatorIds?: string, limit?: number, offset?: number }): Promise<VideoContent[]> {
         try {
             const options: FindManyOptions<VideoContent> = {
-                skip: query.offset,
-                take: query.limit,
                 where: {},
+                take: searchParams.limit,
+                skip: searchParams.offset,
+                relations: ['tags', 'protocols']
             };
-
-            if (query.title) {
-                options.where = { title: Like(`%${query.title}%`) };
-            }
-            options.relations = ['tags', 'protocols'];
-            const [videoContents, count] = await videoContentRepository.findAndCount(options);
-            return { count, videoContents };
+    
+            if (searchParams.creatorIds && searchParams.creatorIds.length > 0) {
+                 const creatorIds = searchParams.creatorIds.split(',');
+                    options.where = {
+                        ...options.where,
+                        userId: In(creatorIds)
+                    };
+                }
+            
+            const videoContents = await videoContentRepository.find(options);
+            return videoContents;
         } catch (error) {
-            console.error('Error listing video content:', error);
+            console.error('Error fetching video contents:', error);
             throw error;
         }
-    }
-
+    }        
+    
     /**
      * Updates video content by ID for a given creator ID
      * @param creatorId The ID of the creator
@@ -113,29 +119,47 @@ export class VideoContentService {
      * @param data Partial data containing fields to update
      * @returns The updated video content
      */
-    public async update(creatorId: number, videoContentId: number, data: Partial<VideoContent>): Promise<VideoContent> {
+    public async update(creatorId: string, videoContentId: string, data: Partial<VideoContent>): Promise<VideoContent> {
         try {
-            const videoContent = await videoContentRepository.findOne({ where: { userId: creatorId, id: videoContentId } });
+            const videoContent = await videoContentRepository.findOne({
+                where: { userId: creatorId, id: videoContentId },
+                relations: ['tags', 'protocols']
+            });            
             if (!videoContent) {
                 throw new NoRecordFoundError(MESSAGES.VIDEO_NOT_EXIST);
             }
+    
             const videoTitleQuery = videoContentRepository.createQueryBuilder('videoContent')
                 .where('videoContent.title = :title', { title: data.title })
                 .andWhere('videoContent.id != :videoContentId', { videoContentId })
                 .andWhere('videoContent.userId = :userId', { userId: creatorId });
-
+    
             const videoTitleExist = await videoTitleQuery.getOne();
             if (videoTitleExist) {
                 throw new DuplicateRecordFoundError(MESSAGES.VIDEO_TITLE_EXIST);
-            }
+            }  
+            // Update the properties of the videoContent object
             Object.assign(videoContent, data);
+
+            // Update tags if provided in newData
+            if (data.tags) {
+                videoContent.tags = data.tags;
+            }
+            // Update protocols if provided in newData
+            if (data.protocols) {
+                videoContent.protocols = data.protocols;
+            }
+            // Save the updated videoContent including its relations
             await videoContentRepository.save(videoContent);
+    
             return videoContent;
         } catch (error) {
             console.error('Error updating video content:', error);
             throw error;
         }
     }
+    
+    
 
     /**
      * Deletes video content by ID for a given creator ID
@@ -143,7 +167,7 @@ export class VideoContentService {
      * @param videoContentId The ID of the video content to delete
      * @returns Success message if video content is deleted successfully
      */
-    public async delete(creatorId: number, videoContentId: number): Promise<{ success: boolean } | { error: string }> {
+    public async delete(creatorId: string, videoContentId: string): Promise<{ success: boolean } | { error: string }> {
         try {
             const videoContent = await videoContentRepository.findOne({ where: { userId: creatorId, id: videoContentId } });
             if (!videoContent) {
