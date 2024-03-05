fe999b16f83154805c3fb14975c02436e727d7a4
Tirthankar231 <tirthankar@wemotiveforge.com>
2024-03-05 14:50:40 +0530

    CHG : Changed the type of userId to string and added extra fields

--- src/api/models/vote.model.ts
+++ src/api/models/vote.model.ts
@@ -7,22 +7,19 @@ export class Vote {
   id: string = uuidv4();
 
   @Column({ nullable: false })
-  userId: number = 0;
+  userId: string = '';
+
+  @Column({ nullable: false })
+  contentId: string = '';
 
   @Column({ type: 'enum', enum: ['UpVote', 'DownVote'], nullable: false })
   voteType: 'UpVote' | 'DownVote' = 'UpVote';
-
-  @Column({ nullable: false, default: 0 })
-  upVote: number = 0;
-
-  @Column({ nullable: false, default: 0 })
-  downVote: number = 0;
 
   @CreateDateColumn({ type: 'bigint', default: () => 'EXTRACT(EPOCH FROM NOW()) * 1000' })
   createdAt: number = Date.now();
 
   @UpdateDateColumn({ type: 'bigint', nullable: false, default: () => 'EXTRACT(EPOCH FROM NOW()) * 1000' })
-  updatedAt: number = Date.now(); 
+  updatedAt: number = Date.now();
 
   @BeforeUpdate()
   updateTimestamp() {
