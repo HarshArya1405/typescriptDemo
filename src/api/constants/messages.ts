c9de60f428590348401f9fe89d650b50aa2897a5
Tirthankar231 <tirthankar@wemotiveforge.com>
2024-03-05 14:48:41 +0530

    ADD : Added message for Texts

--- src/api/constants/messages.ts
+++ src/api/constants/messages.ts
@@ -4,4 +4,5 @@ USER_NAME_EXIST:'A User with given UserName is already exist in system !',
 USER_NOT_EXIST:'A User with given UserName is not exist in system !',
 VIDEO_NOT_EXIST:'A Video with given detail is not exist in system !',
 VIDEO_TITLE_EXIST:'A Video with given title is already exist in system !',
+TEXT_NOT_EXIST: 'A Text with the given details does not exist in the system'
 };
