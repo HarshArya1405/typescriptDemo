76d7fdb54d61789b87fa0397ab91a7784ec2ac12
Tirthankar231 <tirthankar@wemotiveforge.com>
2024-03-05 14:07:58 +0530

    ADD : Added ManyToOne Relationship

--- src/api/models/socialHandle.model.ts
+++ src/api/models/socialHandle.model.ts
@@ -1,5 +1,6 @@
-import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, BeforeUpdate } from 'typeorm';
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, BeforeUpdate, ManyToOne, JoinColumn } from 'typeorm';
 import { v4 as uuidv4 } from 'uuid';
+import { User } from './user.model';
 
 @Entity()
 export class SocialHandle {
@@ -14,6 +15,10 @@ export class SocialHandle {
   
   @Column({ nullable: true })
   url: string = '';
+
+  @ManyToOne(() => User, user => user.socialHandles)
+  @JoinColumn({ name: 'userId' })
+  user!: User;
 
   @CreateDateColumn({ type: 'bigint', default: () => 'EXTRACT(EPOCH FROM NOW()) * 1000' })
   createdAt: number = Date.now();
