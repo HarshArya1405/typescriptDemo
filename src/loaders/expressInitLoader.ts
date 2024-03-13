8af978b2231f71723317a1d198077cd19526c743
HarshArya1405 <harsh@wemotiveforge.com>
2024-03-13 19:15:34 +0530

    CHG : code merged

--- src/loaders/expressInitLoader.ts
+++ src/loaders/expressInitLoader.ts
@@ -1,13 +1,29 @@
 import { MicroframeworkLoader, MicroframeworkSettings } from 'microframework-w3tec';
 import { createExpressServer } from 'routing-controllers';
 import { UserController, RoleController, ProtocolController, TagController, AuthenticationController, VideoContentController, VoteController, YoutubeController, WalletController } from '../api/v1/controllers';
-import Server from '../app';
+import express from 'express';
+import { CorsOptions } from 'cors';
+import ENV from './../config/environments';
+import { requestLogger } from './../util/request.logger';
 
 // Define a loader for initializing Express server with routing-controllers
 export const expressInitLoader: MicroframeworkLoader = (settings?: MicroframeworkSettings) => {
-  // Create an Express server using routing-controllers
+ 
+  const corsOptions: CorsOptions = {
+    origin: (origin, callback) => {
+      if (!origin || ENV.whitelistUrls.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: ENV.methods,
+    credentials: true,
+    exposedHeaders: 'set-cookie'
+  };
+
   const app = createExpressServer({
-    // Specify the controllers to be registered with routing-controllers
+    cors: corsOptions,
     controllers: [
       UserController,
       RoleController,
@@ -20,9 +36,10 @@ export const expressInitLoader: MicroframeworkLoader = (settings?: Microframewor
       WalletController
     ],
   });
-
-  // Initialize the server instance with the Express app
-  new Server(app);
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  // Logging incoming requests
+  app.use(requestLogger);
   // Store the Express app in Microframework settings for later access
   settings?.setData('express_app', app);
 };
