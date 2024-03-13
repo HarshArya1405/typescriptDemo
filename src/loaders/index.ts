bab6107a22ee6455892b0dda36c3b48e3b5a7e96
Tirthankar231 <tirthankar@wemotiveforge.com>
2024-03-13 17:21:40 +0530

    ADD : Exported Swagger loader

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -2,3 +2,4 @@ export {expressInitLoader} from './expressInitLoader';
 export {expressServeLoader} from './expressServeLoader';
 export {auth0Loader} from './auth0Loader';
 export {baseLoader} from './baseLoader';
+export {swaggerLoader} from './swaggerLoader';
