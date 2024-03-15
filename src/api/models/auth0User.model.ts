f05515862aae741e29f3a5efef6f915c994cbe8b
Tirthankar231 <tirthankar@wemotiveforge.com>
2024-03-15 16:20:59 +0530

    ADD : Added auth0User model

---
+++ src/api/models/auth0User.model.ts
@@ -0,0 +1,56 @@
+import { Entity, PrimaryGeneratedColumn, Column, CreateDateColumn, UpdateDateColumn, BeforeUpdate , ManyToOne} from 'typeorm';
+import { v4 as uuidv4 } from 'uuid';
+import { User } from './user.model';
+
+@Entity()
+export class auth0User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string = uuidv4();
+
+  @Column({ nullable: true })
+  fullName: string = '';
+
+  @Column({ nullable: true })
+  userId: string = '';
+  
+  @Column({ nullable: true })
+  userName: string = '';
+  
+  @Column({ nullable: true })
+  email: string = '';
+
+  @Column({ nullable: true })
+  profilePicture: string = '';
+  
+  @Column({ nullable: true })
+  phone: string = '';
+
+  @Column({ nullable: true })
+  title: string = '';
+
+  @Column({ nullable: true })
+  biography: string = '';
+
+  @Column({ nullable: false })
+  role: string = '';
+
+  @Column({ nullable: true })
+  gender: string = '';
+
+  @Column({ nullable: false })
+  sub: string = '';
+
+  @ManyToOne(() => User, user => user.auth0Users)
+  user!: User;
+
+  @CreateDateColumn({ type: 'bigint', default: () => 'EXTRACT(EPOCH FROM NOW()) * 1000' })
+  createdAt: number = Date.now();
+
+  @UpdateDateColumn({ type: 'bigint', nullable: false, default: () => 'EXTRACT(EPOCH FROM NOW()) * 1000' })
+  updatedAt: number = Date.now(); 
+
+  @BeforeUpdate()
+  updateTimestamp() {
+    this.updatedAt = Date.now();
+  }
+}
