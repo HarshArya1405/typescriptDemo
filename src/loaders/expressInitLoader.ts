f02432a49943d91ca9ddfc1496855d38e50d686f
Tirthankar231 <tirthankar@wemotiveforge.com>
2024-03-15 16:29:51 +0530

    CHG : Loaded the AuthUserController

--- src/loaders/expressInitLoader.ts
+++ src/loaders/expressInitLoader.ts
@@ -1,6 +1,6 @@
 import { MicroframeworkLoader, MicroframeworkSettings } from 'microframework-w3tec';
 import { createExpressServer } from 'routing-controllers';
-import { UserController, RoleController, ProtocolController, TagController, AuthenticationController, VideoContentController, VoteController, YoutubeController, WalletController } from '../api/v1/controllers';
+import { UserController, RoleController, ProtocolController, TagController, AuthenticationController, VideoContentController, VoteController, YoutubeController, WalletController, AuthUserController } from '../api/v1/controllers';
 import express from 'express';
 import { CorsOptions } from 'cors';
 import ENV from './../config/environments';
@@ -33,7 +33,8 @@ export const expressInitLoader: MicroframeworkLoader = (settings?: Microframewor
       VideoContentController,
       VoteController,
       YoutubeController,
-      WalletController
+      WalletController,
+      AuthUserController
     ],
   });
   app.use(express.json());
